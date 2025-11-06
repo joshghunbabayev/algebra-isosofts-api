9e58c7fc9d64fe91dd1ea4d68d10805397c0d200
coshgunbabayev <coshgunbabayevw13@gmail.com>
2025-11-07 01:04:51 +0400

    ao;sbfoiasodibsaoidbosaibdoisabdosaasdasddasda

--- types/registers/br.go
+++ types/registers/br.go
@@ -3,21 +3,21 @@ package registerTypes
 import tableComponentTypes "algebra-isosofts-api/types/tableComponents"
 
 type Br struct {
-	Id                     string `json:"id"`
-	No                     string `json:"no"`
-	Swot                   tableComponentTypes.DropDownListItem
-	Pestle                 tableComponentTypes.DropDownListItem
-	InterestedParty        tableComponentTypes.DropDownListItem
-	RiskOpportunity        string `json:"riskOpportunity"`
-	Objective              string `json:"objective"`
-	KPI                    string `json:"kpi"`
-	Process                tableComponentTypes.DropDownListItem
-	ERMEOA                 string `json:"ermeoa"`
-	InitialRiskSeverity    int8   `json:"initialRiskSeverity"`
-	InitialRiskLikelyhood  int8   `json:"initialRiskLikelyhood"`
-	ResidualRiskSeverity   int8   `json:"residualRiskSeverity"`
-	ResidualRiskLikelyhood int8   `json:"residualRiskLikelyhood"`
-	Actions                []string
+	Id                     string                               `json:"id"`
+	No                     string                               `json:"no"`
+	Swot                   tableComponentTypes.DropDownListItem `json:"swot"`
+	Pestle                 tableComponentTypes.DropDownListItem `json:"pestle"`
+	InterestedParty        tableComponentTypes.DropDownListItem `json:"interestedParty"`
+	RiskOpportunity        string                               `json:"riskOpportunity"`
+	Objective              string                               `json:"objective"`
+	KPI                    string                               `json:"kpi"`
+	Process                tableComponentTypes.DropDownListItem `json:"process"`
+	ERMEOA                 string                               `json:"ermeoa"`
+	InitialRiskSeverity    int8                                 `json:"initialRiskSeverity"`
+	InitialRiskLikelyhood  int8                                 `json:"initialRiskLikelyhood"`
+	ResidualRiskSeverity   int8                                 `json:"residualRiskSeverity"`
+	ResidualRiskLikelyhood int8                                 `json:"residualRiskLikelyhood"`
+	Actions                []string                             `json:"actions"`
 }
 
 func (br Br) IsEmpty() bool {
