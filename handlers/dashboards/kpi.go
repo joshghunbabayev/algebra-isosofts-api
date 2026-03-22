25f46b7ac662889b8a6a99438b1b6071b56ff5f0
coshgunbabayev <coshgunbabayevw13@gmail.com>
2026-03-22 22:38:09 +0400

    asdasdsadasdddasdasss

--- handlers/dashboards/kpi.go
+++ handlers/dashboards/kpi.go
@@ -4,7 +4,6 @@ import (
 	"algebra-isosofts-api/middlewares"
 	dashboardModels "algebra-isosofts-api/models/dashboards"
 	registerModels "algebra-isosofts-api/models/registers"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -40,14 +39,94 @@ func (*KPIHandler) GetAll(c *gin.Context) {
 			})
 
 			for _, br := range brs {
-				fmt.Println(br)
 				if br.ResidualRiskSeverity*br.ResidualRiskLikelihood >= 12 {
 					calculatedValue++
 				}
 			}
-		case 2:
-			// calculatedValue = 200
-		case 3:
+		case 2: // Number of Residual High H&S Risks
+			var hsrModel registerModels.HSRModel
+
+			hsrs, _ := hsrModel.GetAll(map[string]interface{}{
+				"dbStatus":  "active",
+				"companyId": account.CompanyId,
+			})
+
+			for _, hsr := range hsrs {
+				if hsr.ResidualRiskSeverity*hsr.ResidualRiskLikelihood >= 12 {
+					calculatedValue++
+				}
+			}
+		case 3: // Number of Legal Requirment with Residual High Risk
+			var legModel registerModels.LEGModel
+
+			legs, _ := legModel.GetAll(map[string]interface{}{
+				"dbStatus":  "active",
+				"companyId": account.CompanyId,
+			})
+
+			for _, leg := range legs {
+				if leg.ResidualRiskSeverity*leg.ResidualRiskLikelihood >= 12 {
+					calculatedValue++
+				}
+			}
+		case 4: // Number of E&A Aspects with High Residual Significance Level
+			var eaiModel registerModels.EAIModel
+
+			eais, _ := eaiModel.GetAll(map[string]interface{}{
+				"dbStatus":  "active",
+				"companyId": account.CompanyId,
+			})
+
+			for _, eai := range eais {
+				if eai.RDOSProbability*eai.RDOSSeverity*eai.RDOSDuration*eai.RDOSScale >= 80 {
+					calculatedValue++
+				}
+			}
+		case 5: // Equipment Safety Rate %
+			var eiModel registerModels.EIModel
+
+			eis, _ := eiModel.GetAll(map[string]interface{}{
+				"dbStatus":  "active",
+				"companyId": account.CompanyId,
+			})
+
+			eisSafeties, _ := eiModel.GetAll(map[string]interface{}{
+				"dbStatus":  "active",
+				"companyId": account.CompanyId,
+				"eis":       1,
+			})
+
+			if len(eis) > 0 {
+				calculatedValue = int64(float64(len(eisSafeties)) / float64(len(eis)) * 100)
+			} else {
+				calculatedValue = 0
+			}
+		case 6: // Training Validity Rate %
+			var traModel registerModels.TRAModel
+
+			tras, _ := traModel.GetAll(map[string]interface{}{
+				"dbStatus":  "active",
+				"companyId": account.CompanyId,
+			})
+
+			trasSafeties, _ := traModel.GetAll(map[string]interface{}{
+				"dbStatus":  "active",
+				"companyId": account.CompanyId,
+				"tras":      1,
+			})
+
+			if len(tras) > 0 {
+				calculatedValue = int64(float64(len(trasSafeties)) / float64(len(tras)) * 100)
+			} else {
+				calculatedValue = 0
+			}
+		case 7:
+			// calculatedValue = 300
+		case 8:
+			// calculatedValue = 300
+		case 9:
+			// calculatedValue = 300
+		case 10:
 			// calculatedValue = 300
 		default:
 			calculatedValue = 0
